49bf8866250950e284f1762dd870de50223d4574
Igcabr01 <igcabr01@ucm.es>
2026-09-17 18:54:49 +0200

    add svg to web redirection + fixing visual bugs

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -19,10 +19,17 @@ function ProjectCard({project}) {
                     </div>
                     <div className="project-links">
                         <div className="project-link-repo">
-                            {<a href={links.repo} className={repoAvailable ? 'petcare-btn-primary' : 'petcare-btn-not'} target="_blank">{textRepo}</a>}
+                            {<a href={links.repo} className={repoAvailable ? 'project-btn-primary' : 'petcare-btn-not'} target="_blank">{textRepo}</a>}
                         </div>
                         <div className={repoAvailable ? 'project-link-web' : 'hidden-element'}>
-                            {<a href={links.web} className='petcare-btn-primary' target="_blank">Ir a la web</a>}
+                            {<a href={links.web} className='project-web-redirect' target="_blank">
+                                Ir a la web
+                                <svg xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-up-right">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M17 7l-10 10" />
+                                    <path d="M8 7l9 0l0 9" />
+                                </svg>
+                            </a>}
                         </div>
                     </div>
 
